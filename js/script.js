fa63ae0c6b461411ba9d30dfacb6bea37c6e0897
Sanampreet Singh <106900639+SanampreetSingh@users.noreply.github.com>
2025-06-23 18:20:02 +0530

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,52 @@
+(function (global) {
+  var dc = {};
+
+  var homeHtmlUrl = "snippets/home-snippet.html";
+  var allCategoriesUrl = "https://coursera-jhu-default-rtdb.firebaseio.com/categories.json";
+  var menuItemsUrl = "https://coursera-jhu-default-rtdb.firebaseio.com/menu_items/{{short_name}}.json";
+
+  var insertHtml = function (selector, html) {
+    document.querySelector(selector).innerHTML = html;
+  };
+
+  var showLoading = function (selector) {
+    var html = "<div class='text-center'><img src='images/ajax-loader.gif'></div>";
+    insertHtml(selector, html);
+  };
+
+  var insertProperty = function (string, propName, propValue) {
+    var propToReplace = "{{" + propName + "}}";
+    return string.replace(new RegExp(propToReplace, "g"), propValue);
+  };
+
+  document.addEventListener("DOMContentLoaded", function () {
+    showLoading("#main-content");
+    $ajaxUtils.sendGetRequest(
+      allCategoriesUrl,
+      function (categories) {
+        var randomCategory = chooseRandomCategory(categories);
+        $ajaxUtils.sendGetRequest(
+          homeHtmlUrl,
+          function (homeHtml) {
+            var finalHtml = insertProperty(homeHtml, "randomCategoryShortName", randomCategory.short_name);
+            insertHtml("#main-content", finalHtml);
+          },
+          false
+        );
+      },
+      true
+    );
+  });
+
+  function chooseRandomCategory(categories) {
+    var index = Math.floor(Math.random() * categories.length);
+    return categories[index];
+  }
+
+  dc.loadMenuItems = function (categoryShortName) {
+    alert("Loading menu for category: " + categoryShortName);
+    // Optional: You could add actual logic to load menu items here.
+  };
+
+  global.$dc = dc;
+})(window);
